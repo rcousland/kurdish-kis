d972fc44c419e204a512d3f19bc42679757ca49c
rcousland <rcousland@gmail.com>
2016-07-08 12:23:48 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,16 @@
+
+// enable smooth scroll
+$(function() {
+  $('a[href*="#"]:not([href="#"], [href="#home"], [href="#menu1"], [href="#menu2"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
